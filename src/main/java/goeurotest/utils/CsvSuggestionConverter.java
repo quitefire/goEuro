66eb1afd74ef562fcef12d86b6403d087b688426
quitefire <b.handozhynski@gmail.com>
2016-11-08 17:39:27 +0200

    added builder to csvsuggestion dto

--- src/main/java/goeurotest/utils/CsvSuggestionConverter.java
+++ src/main/java/goeurotest/utils/CsvSuggestionConverter.java
@@ -1,4 +1,5 @@
 package goeurotest.utils;
+
 import goeurotest.domain.Suggestion;
 import goeurotest.dto.CsvSuggestionDto;
 import lombok.NonNull;
@@ -7,12 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class CsvSuggestionConverter {
     public CsvSuggestionDto toCsvSuggestionDto(@NonNull Suggestion input) {
-        CsvSuggestionDto dto = new CsvSuggestionDto();
-        dto.setId(input.getId());
-        dto.setName(input.getName());
-        dto.setType(input.getType());
-        dto.setLatitude(input.getGeoPosition().getLatitude());
-        dto.setLongitude(input.getGeoPosition().getLongitude());
-        return dto;
+        return new CsvSuggestionDto().builder()
+                .id(input.getId())
+                .name(input.getName())
+                .type(input.getType())
+                .latitude(input.getGeoPosition().getLatitude())
+                .longitude(input.getGeoPosition().getLongitude())
+                .build();
     }
 }
